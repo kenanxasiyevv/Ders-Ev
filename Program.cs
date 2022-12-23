02dc856b8b92325820285f48d0263fec697df64f
kenanxasiyevv <kananakh@code.edu.az>
2022-12-23 15:08:56 +0400

    dersev

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ders_Ev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("A-ni daxil et");
+
+                double a = double.Parse(Console.ReadLine());
+            Console.Write("B-ni daxil et");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = a + b;
+            Console.WriteLine($"{a} + {b} = {c}");
+            
+            
+
+            Console.WriteLine("Congurlations!");
+        }
+    }
+}
